9743afd6cb394ecf55bd3668fa88402bdb174875
Daniel Lee <lee.daniel98@outlook.com>
2017-07-08 11:23:32 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
+import pygame
+from menu import *
 from dealer import Dealer
 
+for x in range(10):
+    dealer = Dealer()
+    dealer.add_player("test")
+    dealer.add_player("test2")
+    dealer.deal_cards()
+    for player in dealer.players:
+        print(player.hand)
+    dealer.inspect_players()
+    
+#windowSurface = pygame.display.set_mode((500, 400), 0, 32)
 
-dealer = Dealer()
-dealer.add_player("test")
-dealer.add_player("test2")
-dealer.deal_cards()
-for player in dealer.players:
-    print(player.hand)
-dealer.inspect_players()
+#menu = MainMenu(windowSurface)
+#menu.run()
